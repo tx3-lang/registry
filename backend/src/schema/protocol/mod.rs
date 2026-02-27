df9b1de7aa38c4f51d69fc8df235bf9d4014f22c
Maximiliano Duthey <maxi.duthey@txpipe.io>
2026-02-27 12:11:36 -0300

    feat: add tx3-tir dependency and update protocol handling in transactions

--- backend/src/schema/protocol/mod.rs
+++ backend/src/schema/protocol/mod.rs
@@ -4,6 +4,7 @@ use async_graphql::{ComplexObject, Enum, SimpleObject, ID};
 
 mod query;
 pub use query::ProtocolQuery;
+use tx3_tir::reduce::Apply;
 
 use crate::ast_to_svg;
 
@@ -45,26 +46,32 @@ pub struct Tx {
 impl Protocol {
     async fn transactions(&self) -> Vec<Tx> {
         let mut txs = vec![];
-        let protocol = tx3_lang::Protocol::from_string(self.source.clone().unwrap()).load().unwrap();
-        for tx in protocol.txs() {
-            let prototx = protocol.new_tx(&tx.name.value).unwrap();
+        let code = self.source.clone().unwrap();
+        let mut protocol = tx3_lang::parsing::parse_string(&code).unwrap();
+        tx3_lang::analyzing::analyze(&mut protocol);
+        for tx in &protocol.txs {
+            let tx_tir = tx3_lang::lowering::lower(&protocol, &tx.name.value.clone()).unwrap();
             let mut parameters: Vec<TxParam> = Vec::new();
-            for param in prototx.find_params() {
+            for (name, ty) in tx_tir.params() {
                 parameters.push(TxParam {
-                    name: param.0.clone(),
-                    r#type: format!("{:?}", param.1),
+                    name: name.clone(),
+                    r#type: format!("{:?}", ty.clone()),
                     // TODO: Add description when supported in tx3-lang
                     description: None,
                 });
             }
             parameters.sort_by_key(|p| p.name.clone());
+            let tx_tir = tx3_lang::lowering::lower(&protocol, &tx.name.value.clone()).unwrap();
+
+            let (tx_bytes, version) = tx3_tir::encoding::to_bytes(&tx_tir);
+
             txs.push(Tx {
                 name: tx.name.value.clone(),
                 // TODO: Add description when supported in tx3-lang
                 description: None,
                 parameters,
-                tir: hex::encode(prototx.ir_bytes()),
-                tir_version: tx3_lang::ir::IR_VERSION.to_string(),
+                tir: hex::encode(tx_bytes),
+                tir_version: version.to_string(),
                 protocol_source: self.source.clone(),
             });
         }
@@ -76,10 +83,9 @@ impl Protocol {
 impl Tx {
     async fn svg(&self) -> Option<String> {
         if let Some(source) = &self.protocol_source {
-            let protocol = tx3_lang::Protocol::from_string(source.clone()).load().unwrap();
-            let ast= protocol.ast();
-            let tx_def = protocol.txs().find(|t| t.name.value == self.name)?;
-            Some(ast_to_svg::tx_to_svg(ast, tx_def, self.parameters.iter().map(|p| p.name.clone()).collect()))
+            let ast = tx3_lang::parsing::parse_string(source).unwrap();
+            let tx_def = ast.txs.iter().find(|t| t.name.value == self.name)?;
+            Some(ast_to_svg::tx_to_svg(&ast, tx_def, self.parameters.iter().map(|p| p.name.clone()).collect()))
         } else {
             None
         }
