b96cae49799a792e4b2f30e60c10ea3d8e4a64ec
Maximiliano Duthey <maxi.duthey@txpipe.io>
2026-05-18 15:35:25 -0300

    feat(backend): wire DATABASE_URL into the backend bootstrap module (#12)

--- bootstrap/backend/deployment.tf
+++ bootstrap/backend/deployment.tf
@@ -35,6 +35,35 @@ resource "kubernetes_deployment" "this" {
             value = var.registry_protocol
           }
 
+          env {
+            name = "PG_USER"
+            value_from {
+              secret_key_ref {
+                name = var.db_credentials_secret
+                key  = "username"
+              }
+            }
+          }
+
+          env {
+            name = "PG_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = var.db_credentials_secret
+                key  = "password"
+              }
+            }
+          }
+
+          # Zalando operator passwords are not guaranteed URL-safe. If a generated
+          # password contains ':', '@', '/' or '?', this connection string will be
+          # malformed. Mitigation if it ever bites: switch to PgConnectOptions or
+          # URL-encode PG_PASSWORD in an init container.
+          env {
+            name  = "DATABASE_URL"
+            value = "postgresql://$(PG_USER):$(PG_PASSWORD)@${var.db_host}:${var.db_port}/${var.db_name}?sslmode=disable"
+          }
+
           resources {
             limits   = var.resources.limits
             requests = var.resources.requests
